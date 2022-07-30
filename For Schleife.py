409b76cf1612b5cd40be72d1cac50b3308840550
Florian <github@flow96.de>
2022-07-31 01:20:29 +0200

    Zeile 18 bis 20 sollen erwirken, dass bei negativer Eingabe in Zeile 6 das Programm wieder von vorne läuft

--- For Schleife.py
+++ For Schleife.py
@@ -14,9 +14,10 @@ if rezept == int(2):
     print("Du hast das Rezept für Waffeln gewählt.")
 if rezept == int(3):
     print("Du hast das Rezept für Käsekuchen gewählt.")
-else:
-    print(
-        "Fehlerhafte Eingabe. Bitte erneut versuchen." + os.execl(sys.executable, os.path.abspath(__file__), *sys.argv))
+
+# else:
+#     print(
+#         "Fehlerhafte Eingabe. Bitte erneut versuchen." + os.execl(sys.executable, os.path.abspath(__file__), *sys.argv))
 
 if rezept == 1:
     for pfannkuchen in ["\nDu benötigst:\n \n"
