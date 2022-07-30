4781e9400f25171bea3cda2f3630dc43a6c52d03
Florian <github@flow96.de>
2022-07-31 01:18:37 +0200

    Zeile 11, 13 und 15 auf int gesetzt

--- For Schleife.py
+++ For Schleife.py
@@ -4,46 +4,47 @@ import sys
 portions = 6
 
 rezept = int(input("[1] Pfannkuchen\n"
-               "[2] Waffeln\n"
-               "[3] Käsekuchen\n"
-               "Bitte wähle eins der folgenden Rezepte aus: "))
+                   "[2] Waffeln\n"
+                   "[3] Käsekuchen\n"
+                   "Bitte wähle eins der folgenden Rezepte aus: "))
 
-if rezept == 1:
+if rezept == int(1):
     print("Du hast das Rezept für Pfannkuchen gewählt.")
-if rezept == 2:
+if rezept == int(2):
     print("Du hast das Rezept für Waffeln gewählt.")
-if rezept == 3:
+if rezept == int(3):
     print("Du hast das Rezept für Käsekuchen gewählt.")
 else:
-    print("Fehlerhafte Eingabe. Bitte erneut versuchen." + os.execl(sys.executable, os.path.abspath(__file__), *sys.argv))
+    print(
+        "Fehlerhafte Eingabe. Bitte erneut versuchen." + os.execl(sys.executable, os.path.abspath(__file__), *sys.argv))
 
 if rezept == 1:
     for pfannkuchen in ["\nDu benötigst:\n \n"
-          "2 Eier,\n"
-          "60 ml Mineralwasser, \n"
-          "200 g Mehl, \n"
-          "1 Prise Zucker, \n" 
-          "1 Prise Salz, \n" 
-          "200 ml Milch"]:
+                        "2 Eier,\n"
+                        "60 ml Mineralwasser, \n"
+                        "200 g Mehl, \n"
+                        "1 Prise Zucker, \n"
+                        "1 Prise Salz, \n"
+                        "200 ml Milch"]:
         print(pfannkuchen)
 
 if rezept == 2:
     for waffeln in ["\nDu benötigst:\n\n"
-          "3 Eier,\n"
-          "125 g Butter,\n"
-          "100 g Zucker, \n"
-          "1 Packung Vanillezucker, \n"
-          "250 g Mehl, \n"
-          "1 Prise Salz, \n" 
-          "1 TL Backpulver, \n" 
-          "200 ml Milch"]:
+                    "3 Eier,\n"
+                    "125 g Butter,\n"
+                    "100 g Zucker, \n"
+                    "1 Packung Vanillezucker, \n"
+                    "250 g Mehl, \n"
+                    "1 Prise Salz, \n"
+                    "1 TL Backpulver, \n"
+                    "200 ml Milch"]:
         print(waffeln)
 
 if rezept == 3:
     for käsekuchen in ["\nDu benötigst:\n\n"
-          "1 Ei,\n"
-          "75 g Zucker,\n"
-          "75 g Margarine,\n"
-          "200 g Mehl, \n"
-          "1/2 Päckchen Backpulver"]:
+                       "1 Ei,\n"
+                       "75 g Zucker,\n"
+                       "75 g Margarine,\n"
+                       "200 g Mehl, \n"
+                       "1/2 Päckchen Backpulver"]:
         print(käsekuchen)
